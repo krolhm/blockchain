b145978c35842a49491577db2db42cde8b4b36bd
Raphael bourgeat <cosmosraph@gmail.com>
2021-08-15 01:28:09 +0200

    simple blockchain done

---
+++ src/Blockchain.h
@@ -0,0 +1,24 @@
+#ifndef KROLHM_BLOCKCHAIN_H
+#define KROLHM_BLOCKCHAIN_H
+
+#include <cstdint>
+#include <vector>
+#include "Block.h"
+
+using namespace std;
+
+class Blockchain
+{
+    public:
+        Blockchain();
+
+        void AddBlock(Block bNew);
+
+    private:
+        uint32_t _nDifficulty;
+        vector<Block> _vChain;
+
+        Block _GetLastBlock() const;
+};
+
+#endif
